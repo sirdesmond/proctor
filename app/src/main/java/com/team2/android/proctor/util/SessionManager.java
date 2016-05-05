acfc490d847c6dc465b7e562fbdfea2818fefa82
akedari <akedari@hawk.iit.edu>
2016-05-05 18:41:52 -0500

    Logout issue

--- app/src/main/java/com/team2/android/proctor/util/SessionManager.java
+++ app/src/main/java/com/team2/android/proctor/util/SessionManager.java
@@ -94,6 +94,8 @@ public class SessionManager {
         // Add new Flag to start new Activity
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 
+        // Staring Login Activity
+        _context.startActivity(i);
     }
 
 
